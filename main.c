6994dada68f0f12f3a4580c5d0067a542ecdde49
Verdict33 <tvt6.bel@gmail.com>
2024-09-11 22:16:54 +0300

    diskretka 3.1/1.4/3:11

--- main.c
+++ main.c
@@ -24,24 +24,39 @@ bool isSubset(int A[][2], int sizeA, int B[][2], int sizeB) {
     return true;
 }
 
+bool areEqual(int A[][2], int sizeA, int B[][2], int sizeB) {
+    // Проверяем, является ли A подмножеством B
+    bool A_in_B = isSubset(A, sizeA, B, sizeB);
+    // Проверяем, является ли B подмножеством A
+    bool B_in_A = isSubset(B, sizeB, A, sizeA);
+    return A_in_B && B_in_A;
+}
+
+// Функция для проверки строгого включения A в B
+bool isStrictSubset(int A[][2], int sizeA, int B[][2], int sizeB) {
+    // Проверяем, является ли A подмножеством B
+    bool A_in_B = isSubset(A, sizeA, B, sizeB);
+    // Проверяем, что A и B не равны
+    bool A_not_equal_B = !areEqual(A, sizeA, B, sizeB);
+    return A_in_B && A_not_equal_B;
+}
+
 // Основная функция
 int main() {
-
     SetConsoleOutputCP(CP_UTF8);
-
     // Определяем множество A
-    int A[][2] = {{1, 2}};
+    int A[][2] = {{3, 3},{2, 3}};
     int sizeA = sizeof(A) / sizeof(A[0]);
 
     // Определяем множество B
-    int B[][2] = {{1, 2}, {2, 3}, {3, 4}, {4, 5}};
+    int B[][2] = {{1, 2}, {2, 3}, {3, 4}};
     int sizeB = sizeof(B) / sizeof(B[0]);
 
-    // Проверяем, является ли A подмножеством B
-    if (isSubset(A, sizeA, B, sizeB)) {
-        printf("Множество A является подмножеством множества B.\n");
+    // Проверяем строгое включение A в B
+    if (isStrictSubset(A, sizeA, B, sizeB)) {
+        printf("Множество A строго включено в множество B.\n");
     } else {
-        printf("Множество A не является подмножеством множества B.\n");
+        printf("Множество A не является строгим подмножеством множества B.\n");
     }
 
     return 0;
